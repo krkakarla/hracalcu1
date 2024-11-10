76566d6ed9df3a9b0245f5b7ab6287b5574140a3
krkakarla <krkakarla@gmail.com>
2024-11-10 12:37:34 +0530

    calculator with docker details
    calculator with docker details

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+/*
 public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
@@ -15,5 +16,64 @@ public class Main {
             // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
         }
+    }
+}
+
+*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        char operator;
+        Double number1, number2, result;
+
+        // create an object of Scanner class
+        Scanner input = new Scanner(System.in);
+
+        // ask users to enter operator
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+        // ask users to enter numbers
+        System.out.println("Enter first number");
+        number1 = input.nextDouble();
+
+        System.out.println("Enter second number");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+
+            // performs addition between numbers
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            // performs subtraction between numbers
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+            // performs multiplication between numbers
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+
+            // performs division between numbers
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        input.close();
     }
 }
